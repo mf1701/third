6fe03e50ab54bdcd60d63b4d0b32682913d74227
Administrator <Administrator@A-201704261329>
2017-06-09 16:02:49 +0800

    User getter

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,5 +1,26 @@
 package entity;
 
 public class User {
-
+	private int id;
+	private String account;
+	private String pwd;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAccount() {
+		return account;
+	}
+	public void setAccount(String account) {
+		this.account = account;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	
 }
